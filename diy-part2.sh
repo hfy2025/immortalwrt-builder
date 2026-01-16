e492c8c96f61853048cc08343ff6e8b0f74d0a00
hfy2025 <hongfy2025@gmail.com>
2026-01-16 21:24:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,6 +5,10 @@
 
 echo "========== DIY Part 2 Start =========="
 
+# 修改管理 IP 和主机名
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/X86-Router/g' package/base-files/files/bin/config_generate
+
 # 创建版本信息
 BUILD_DATE=$(date +%Y%m%d-%H%M)
 echo "Build Date: $BUILD_DATE" > bin/targets/x86/64/build-info.txt
